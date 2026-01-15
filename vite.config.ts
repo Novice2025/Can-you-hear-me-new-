141b85b6fec4cc39490289cbf464be6f3dd638e9
Vishul Daby <itailormadeidiomas@gmail.com>
2026-01-15 17:35:50 -0300

    Build for GitHub Pages with docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy' // <--- NEW IMPORT
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    viteStaticCopy({ // <--- NEW PLUGIN CONFIGURATION
+    viteStaticCopy({
       targets: [
-        // Copies 'src/data' to 'dist/data'
-        // Since you moved your files to public/data, this plugin is actually not strictly needed anymore for the JSON files
-        // but it's good to keep if you have other static assets in src/data that you want copied.
-        // For now, we'll keep it to ensure any other potential data files are handled.
-        { src: 'src/data', dest: 'data' } // This line is now set up to copy src/data to dist/data
+        { src: 'src/data', dest: 'data' }
       ]
     })
   ],
-  base: './', // Ensures correct relative paths for assets
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
@@ -29,5 +24,8 @@ export default defineConfig({
         additionalData: `@import "@/assets/styles/_variables.scss";`
       }
     }
+  },
+  build: {
+    outDir: 'docs'
   }
 })
